a8bdf7ccada75916912aac0a2d560268e4e3df0e
SoniaC7 <59957248+SoniaC7@users.noreply.github.com>
2021-02-24 15:25:33 +0100

    Interacció E amb NPC

--- Assets/Creator Kit - RPG/Scripts/Gameplay/NPCController.cs
+++ Assets/Creator Kit - RPG/Scripts/Gameplay/NPCController.cs
@@ -1,6 +1,9 @@
 using RPGM.Core;
 using RPGM.Gameplay;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace RPGM.Gameplay
 {
@@ -11,27 +14,85 @@ namespace RPGM.Gameplay
     {
         public ConversationScript[] conversations;
 
+        public KeyCode interactKey;
+
+        public bool isInRange;
+
+        public bool displayMessage;
+
+        public bool conv_start = false;
+
         Quest activeQuest = null;
 
         Quest[] quests;
 
         GameModel model = Schedule.GetModel<GameModel>();
 
+        public float hideTextDuration = 2.0f;
+
         void OnEnable()
         {
             quests = gameObject.GetComponentsInChildren<Quest>();
         }
-
-        public void OnCollisionEnter2D(Collision2D collision)
+        
+        void Update()
         {
-            var c = GetConversation();
-            if (c != null)
+            if (isInRange)
             {
-                var ev = Schedule.Add<Events.ShowConversation>();
-                ev.conversation = c;
-                ev.npc = this;
-                ev.gameObject = gameObject;
-                ev.conversationItemKey = "";
+                if (!conv_start)
+                {
+                    displayMessage = true;
+                    StartCoroutine(WaitAndMakeTextDisappear(hideTextDuration));
+                }
+
+                if (Input.GetKeyDown(interactKey))
+                {
+                    Debug.Log("Key pressed");
+                    StartConv();
+                }
+            }
+        }
+        private IEnumerator WaitAndMakeTextDisappear(float waitTimeInSeconds)
+        {
+            yield return new WaitForSeconds(1.5f);
+            displayMessage = false;
+        }
+
+        public void StartConv()
+        {
+            conv_start = true;
+            var c = GetConversation();
+                if (c != null)
+                {
+                    var ev = Schedule.Add<Events.ShowConversation>();
+                    ev.conversation = c;
+                    ev.npc = this;
+                    ev.gameObject = gameObject;
+                    ev.conversationItemKey = "";
+                }
+        }
+
+        public void OnTriggerEnter2D(Collider2D collision)
+        {
+                if (collision.gameObject.CompareTag("Player"))
+                {
+                    isInRange = true;
+                    Debug.Log("Player in range");
+                }
+            
+        }
+        
+        public void OnTriggerExit2D(Collider2D collision)
+        {
+                isInRange = false;
+                Debug.Log("Player not in range");
+        }
+        
+        public void OnGUI()
+        {
+            if (displayMessage)
+            {
+                GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 200f, 200f), "Apreta E per parlar");
             }
         }
 

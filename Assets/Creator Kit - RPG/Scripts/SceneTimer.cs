2bb783ec9aa8dbf21b8b85ced6dee4de62c60924
alex <alex.acosta01@estudiant.upf.edu>
2021-02-25 12:12:01 +0100

    Crorrecció de la nova players position
    Arreglem que la posició del player s'actualitzi només després del canvi d'escena. (updated scene_index)

--- Assets/Creator Kit - RPG/Scripts/SceneTimer.cs
+++ Assets/Creator Kit - RPG/Scripts/SceneTimer.cs
@@ -25,7 +25,7 @@ public class SceneTimer : MonoBehaviour //asigned to user to change its spawn po
         player_spawns[1] = new Vector3(10f, 14f, 0f);
         player_spawns[2] = new Vector3(0f, 0f, 0f);
 
-        gameObject.transform.position = player_spawns[0]; //set first player position
+        //gameObject.transform.position = player_spawns[0]; //set first player position
     }
 
     void Update()
@@ -34,9 +34,9 @@ public class SceneTimer : MonoBehaviour //asigned to user to change its spawn po
 
         if (this.time >= this.scene_time) //if arrives at the maximum scene time change the "scene" (player's room)
         {
-            gameObject.transform.position = player_spawns[scene_index]; //update player position
             this.time = 0.0f; //reset time
             scene_index = (scene_index + 1) % total_scenes;
+            gameObject.transform.position = player_spawns[scene_index]; //update player position
             Debug.Log("La escena ha cambiado a la numero "+ scene_index );
         }
     }

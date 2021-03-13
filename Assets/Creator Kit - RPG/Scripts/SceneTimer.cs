99328b3dc25ec45d237a3e8c5877c850eefd3068
alex <alex.acosta01@estudiant.upf.edu>
2021-03-13 18:37:20 +0100

    Temporalitat completa + Tots NPCs/Diálegs Introducció
    Hem afegit un conometre a la GUI amb tota la temporalitat gestionada per mostrar al jugador la temporalitat de les sales, també hem arreglat el problema del canvi d'escena (el npc_collection no es pot seleccionar ja que no conté els objectes npcs, potser si si seleccionesim els fills)
    
    A més ja hem afegit els NPCs i els diálegs de la introducció.

--- Assets/Creator Kit - RPG/Scripts/SceneTimer.cs
+++ Assets/Creator Kit - RPG/Scripts/SceneTimer.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SceneTimer : MonoBehaviour //asigned to user to change its spawn positions depending on scene
+public class SceneTimer : MonoBehaviour //asigned to user to change its spawn positions depending on scene and to the cronometer of the room scene
 {
     public float time = 0.0f;           //time passing by
     public bool using_time = true;      //using time, bool controler
@@ -14,15 +15,37 @@ public class SceneTimer : MonoBehaviour //asigned to user to change its spawn po
     public int total_scenes;
     public int scene_index  = 0;    //actual scene
     public float[] scene_time;      //maximum scene time
-    private Vector3[] player_spawns;
 
-    public GameObject[] npc_collec ;
-    public GameObject npc ;
+    private Vector3[] player_spawns;
+    public GameObject player;       //player object
+
+    public GameObject[] npc_collection;
     public NPCController np;
+
+    //variables to define the GUI of the time
+    private Text timerText;
 
     public void increaseTime (float amount_time)
     {
         this.time += amount_time;
+    }
+
+    private void updateTimer ()
+    {
+        int mins = 0;
+        int secs = 0;
+        int timeRemaining = (int) (this.scene_time[this.scene_index] - time); //get remaining time to secs
+
+        mins = timeRemaining / 60;
+        secs = timeRemaining % 60;
+        secs = Mathf.Max(secs, 0); //stop at zero
+
+        timerText.text = mins.ToString("00") + ":" + secs.ToString("00"); //2 zeros to show only 2 digits
+
+    }
+    private void updatePausedTimer()
+    {
+        timerText.text = ""; //timer GUI not shown
     }
 
     void Start()
@@ -31,42 +54,54 @@ public class SceneTimer : MonoBehaviour //asigned to user to change its spawn po
 
         player_spawns = new Vector3[total_scenes];
         player_spawns[0] = new Vector3(-0.5f, 11f, 0f);
-        player_spawns[1] = new Vector3(10f, 14f, 0f);
+        player_spawns[1] = new Vector3(10f, 15f, 0f);
         player_spawns[2] = new Vector3(10f, -3f, 0f);
 
         //will set the incoming max times
         scene_time = new float[total_scenes];
-        scene_time[0] = 10.0f;
-        scene_time[1] = 10.0f;
-        scene_time[2] = 10.0f;
+        scene_time[0] = 120.0f;
+        scene_time[1] = 300.0f;
+        scene_time[2] = 300.0f;
 
-        npc_collec = GameObject.FindGameObjectsWithTag("npc_collection");
+        npc_collection = GameObject.FindGameObjectsWithTag("npc");  //get all npcs
+        player = GameObject.FindGameObjectsWithTag("Player")[0];    //only there is one, the player
 
-      
-        //gameObject.transform.position = player_spawns[0]; //set first player position
+        //time GUI inizialised
+        timerText = GameObject.FindGameObjectsWithTag("timer")[0].GetComponent<Text>();
+        updateTimer();
     }
 
     void Update()
     {
         if (using_time)
-            this.time += Time.deltaTime;        
-
-        foreach(GameObject npc in npc_collec)
         {
-            np = npc.GetComponent<NPCController>();
+            this.time += Time.deltaTime;
+            updateTimer();
+        }
+        else
+        {
+            updatePausedTimer();
+        }
 
-            if (this.time >= this.scene_time[this.scene_index] && np.conv_start == false) //if arrives at the maximum scene time change the "scene" (player's room)
+        bool chatting = false;
+
+        for (int i = 0; i < npc_collection.Length; i++) //test for all the NPCs
+        {
+            np = npc_collection[i].GetComponent<NPCController>();
+            if (np.conv_start == true)  //if Player is chatting with anyone
             {
-                this.time = 0.0f; //reset time
-                scene_index = (scene_index + 1) % total_scenes;
-                gameObject.transform.position = player_spawns[scene_index]; //update player position
-                Debug.Log("La escena ha cambiado a la numero "+ scene_index );
+                chatting = true;
             }
         }
-        
+
+        if (this.time >= this.scene_time[this.scene_index] && !chatting) //if arrives at the maximum scene time change the "scene" (player's room)
+        {
+            this.time = 0.0f; //reset time
+            scene_index = (scene_index + 1) % total_scenes;
+            player.transform.position = player_spawns[scene_index]; //update player position
+            Debug.Log(player_spawns[scene_index]);
+            Debug.Log("La escena ha cambiado a la numero "+ scene_index );
+        }
           
     }
 }
-
-//gameObject en minuscula se refiere al objeto al que este script esta asignado
-//podemos utilizar esto si por ejemplo quisieramos mover NPCs pero dandoles arrays de spawn diferentes
